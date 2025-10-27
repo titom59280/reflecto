3b5a2fa03f639d3bd242306ceaa8bfc666b6d5bc
thomas.graveleine <thomas.graveleine@relyens.eu>
2025-10-27 08:18:19 +0100

    fix url api

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = process.env.API_BASE_URL;
-const API_BASE = API_BASE_URL + 'api';
+const API_BASE = process.env.API_BASE_URL_API;
 
 const tokenKey = 'retro-token';
 const userKey = 'retro-user';
